ff947c4eb5f7f0d083e53fac72877fde899c564b
keecpe10 <40386831+keecpe10@users.noreply.github.com>
2022-06-15 15:31:42 +0700

    Update pxt.json, main.blocks, main.ts, krathok-main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let kp = 0.09
+let kd = 2.3
+KrathokRobot.InitStart()
+KrathokRobot.ButtonWaite(LineRobotButton.A)
+basic.showIcon(IconNames.Surprised)
+KrathokRobot.SaveValueSensors()
+KrathokRobot.ButtonWaite(LineRobotButton.B)
+basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    KrathokRobot.FollowerLine(800, kp, kd)
 })
